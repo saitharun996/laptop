4dde6a7671452bc94eece0aedb610af23b47018d
saitharun996 <saitharun996@gmail.com>
2022-10-20 06:46:20 +0530

    newly constructed building

---
+++ SDET_9/src/main/java/Constructions/Building_construction.java
@@ -0,0 +1,11 @@
+package Constructions;
+
+public class Building_construction {
+
+	public static void main(String[] args)
+	{
+		System.out.println("The building constructed in Rajendranagar");
+		
+	}
+
+}
