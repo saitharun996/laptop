0611ed09c2b4128beb69b75bae7293f4b2109686
saitharun996 <saitharun996@gmail.com>
2022-10-20 06:48:55 +0530

    updated

--- SDET_9/src/main/java/Constructions/Building_construction.java
+++ SDET_9/src/main/java/Constructions/Building_construction.java
@@ -4,7 +4,7 @@ public class Building_construction {
 
 	public static void main(String[] args)
 	{
-		System.out.println("The building constructed in Rajendranagar");
+		System.out.println("The building constructed in hyderabad");
 		
 	}
 
